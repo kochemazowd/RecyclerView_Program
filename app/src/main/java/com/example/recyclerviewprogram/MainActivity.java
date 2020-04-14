afab27db6fbb05eb8a56e619287226521c04e120
Kochemazowd <kochemazovvd@gmail.com>
2020-04-14 13:13:57 +0400

    creating a list users whith add function

--- app/src/main/java/com/example/recyclerviewprogram/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewprogram/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.recyclerviewprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+    private final RecyclerView.Adapter adapter = new ItemAdapter(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recycler = findViewById(R.id.rv_people_list);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(adapter);
     }
+
+    public void add(View view){
+        Intent intent = new Intent(this.getApplicationContext(), AddUserActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
